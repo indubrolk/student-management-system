320744e9031a9befb11a882f9801c77954a4b2f1
indubrolk <110773586+indubrolk@users.noreply.github.com>
2025-04-13 13:24:30 +0530

    main.cpp committed

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ main() {
     int value;
     while (true) {
         cout<<"Press 1 to enter Data"<<end1;
+        cout<<"Press 2 to show data"<<end1;
+        cout<<"Press 3 to "
 
         cin>>value;
 
@@ -16,6 +18,21 @@ main() {
             case 2:
                 enter();
                 break;
+
+            case 3:
+                enter();
+                break;
+
+            case 4:
+                enter();
+                break;
+
+            case 5:
+                enter();
+                braek;
+
+            case 6:
+                exit(0);
         }
     }
 }
