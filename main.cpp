3e9a431dd01ef8cd7d81ccfd0e8daeb3bd94a6f9
indubrolk <110773586+indubrolk@users.noreply.github.com>
2025-04-13 17:34:32 +0530

    main.cpp committed

--- main.cpp
+++ main.cpp
@@ -1,38 +1,62 @@
 #include<iostream>
 
 using namespace std;
+
+void enter()
+{}
+
+void show()
+{}
+
+void search()
+{}
+
+void update()
+{}
+
+void deleterecord()
+{}
+
 main() {
     int value;
     while (true) {
-        cout<<"Press 1 to enter Data"<<end1;
-        cout<<"Press 2 to show data"<<end1;
-        cout<<"Press 3 to "
+        cout<<"Press 1 to enter Data"<<endl;
+        cout<<"Press 2 to show data"<<endl;
+        cout<<"Press 3 to search data"<<endl;
+        cout<<"Press 4 to update data"<<endl;
+        cout<<"Press 5 to delete data"<<endl;
+        cout<<"Press 6 to exit"<<endl;
 
         cin>>value;
 
-        switch (value) {
+        switch(value) {
             case 1:
                 enter();
                 break;
 
             case 2:
-                enter();
+                show();
                 break;
 
             case 3:
-                enter();
+                search();
                 break;
 
             case 4:
-                enter();
+                update();
                 break;
 
             case 5:
-                enter();
-                braek;
+                deleterecord();
+                break;
 
             case 6:
                 exit(0);
+                break;
+
+            default:
+                cout<<"Invalid Input"<<endl;
+            break;
         }
     }
 }
